fdd68634fb152c53ab7559abb7c65b8ba2a267ca
carolchep <carolchepsam@gmail.com>
2022-06-16 16:29:01 +0300

    IG clone

---
+++ client/src/Data/TrendData.js
@@ -0,0 +1,26 @@
+export const TrendData= [
+  {
+    name: "Wajakoyah",
+    shares: 100,
+  },
+  {
+    name: "Fuel Prices",
+    shares: 90,
+  },
+  {
+    name: "KRA",
+    shares: 70,
+  },
+  {
+    name: "Reactjs",
+    shares: 50,
+  },
+  {
+    name: "Azimio",
+    shares: 40,
+  },
+  {
+    name: "Uhuru kenyatta",
+    shares: 30,
+  },
+];
